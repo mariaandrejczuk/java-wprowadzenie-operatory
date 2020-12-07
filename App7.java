eb75f818c8515543a7eb9e52ab2eb0019a381a73
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2020-12-07 19:44:31 +0100

    #13 program obliczający resztę z dzielenia za pomocą modulo

---
+++ App7.java
@@ -0,0 +1,10 @@
+class App7 {
+    public static void main(String args[]) {
+        double x = 47%10;
+        System.out.println("Reszta z dzielenia 47/10 wynosi: "+x);
+        System.out.println();
+        double y = 35.5%6;
+        System.out.println("Reszta z dzielenia 35.5/6 wynosi: "+y);
+        System.out.println();
+    }
+}
