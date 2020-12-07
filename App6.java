1f4668183e5a299e62861e1fd0f314b6ac64a3e9
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2020-12-07 19:23:23 +0100

    #11 program obliczajacy wartość absolutną liczby x

---
+++ App6.java
@@ -0,0 +1,8 @@
+class App6{
+    public static void main(String args[]) {
+        int x = -5;
+        int xAbsolute;
+        xAbsolute = x<0?-x:x;
+        System.out.println("Wartość absolutna z x=-5 wynosi: "+xAbsolute);
+    }
+}
