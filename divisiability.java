ff9158f58bae83b76c92ce621de55d06a7150ef1
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2020-12-09 15:04:38 +0100

    #15 program sprawdzający podzielność wybranych liczb przez 48

---
+++ divisiability.java
@@ -0,0 +1,13 @@
+class divisiability{
+    public static void main(String args[]) {
+        boolean podzielna;
+        podzielna = 56584%48 == 0? true: false; 
+        System.out.println("Czy liczba 56584  jest podzielna przez 48?: "+podzielna);
+        System.out.println(); 
+        podzielna = 22231%48 == 0? true: false; 
+        System.out.println("Czy liczba 22231 jest podzielna przez 48?: "+podzielna);
+        System.out.println(); 
+        podzielna = 69688454%48 == 0? true: false; 
+        System.out.println("Czy liczba 22231 jest podzielna przez 48?: "+podzielna);
+    }
+}
